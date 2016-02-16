67f53f36fb0969706a2537f86a7fc06fb2e7a289
Gerald Oliva <ryumaster001@yahoo.com>
2016-02-16 14:42:54 -0500

    Assignment 35 completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,6 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+  
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 15 }
 end
