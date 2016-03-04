a564d2858a2ac9b841d69f27afc7d2b4bb1ed412
Gerald Oliva <ryumaster001@yahoo.com>
2016-03-04 12:58:26 -0500

    Checkpoint 42 3/4 completed

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,8 +1,8 @@
- class Label < ActiveRecord::Base
- # #3
-   has_many :labelings
- # #4
-   has_many :topics, through: :labelings, source: :labelable, source_type: :Topic
- # #5
-   has_many :posts, through: :labelings, source: :labelable, source_type: :Post
- end
+class Label < ActiveRecord::Base
+
+  has_many :labelings
+
+  has_many :topics, through: :labelings, source: :labelable, source_type: :Topic
+
+  has_many :posts, through: :labelings, source: :labelable, source_type: :Post
+end
--- spec/models/labeling_spec.rb
+++ spec/models/labeling_spec.rb
@@ -1,6 +1,6 @@
- require 'rails_helper'
- 
- RSpec.describe Labeling, type: :model do
- # #6
-   it { is_expected.to belong_to :labelable }
- end
+require 'rails_helper'
+
+RSpec.describe Labeling, type: :model do
+
+  it { is_expected.to belong_to :labelable }
+end
