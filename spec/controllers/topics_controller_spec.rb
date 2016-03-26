6124939e3483fb3f89c748e06c57a7a2a7cceeea
Gerald Oliva <ryumaster001@yahoo.com>
2016-03-25 21:40:09 -0400

    Checkpoint 46 completed

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -4,6 +4,7 @@ include RandomData
 
 RSpec.describe TopicsController, type: :controller do
   let(:my_topic) { create(:topic) }
+  let(:my_private_topic) { create(:topic, public: false) }
 
   context "guest" do
     describe "GET index" do
@@ -12,13 +13,22 @@ RSpec.describe TopicsController, type: :controller do
         expect(response).to have_http_status(:success)
       end
 
-      it "assigns Topic.all to topic" do
+      it "assigns only public topics to topics" do
         get :index
         expect(assigns(:topics)).to eq ([my_topic])
+      end
+
+      it "does not include private topics in @topics" do
+        get :index
+        expect(assigns(:topics)).not_to include(my_private_topic)
       end
     end
 
     describe "GET show" do
+      it "redirects for private topics" do
+        get :show, {id: my_private_topic.id}
+        expect(response).to redirect_to(new_session_path)
+      end
       it "returns http success" do
         get :show, {id: my_topic.id}
         expect(response).to have_http_status(:success)
@@ -85,9 +95,9 @@ RSpec.describe TopicsController, type: :controller do
         expect(response).to have_http_status(:success)
       end
 
-      it "assigns Topic.all to topic" do
+      it "assigns Topic.all to topics" do
         get :index
-        expect(assigns(:topics)).to eq([my_topic])
+        expect(assigns(:topics)).to eq([my_topic, my_private_topic])
       end
     end
 
@@ -158,9 +168,9 @@ RSpec.describe TopicsController, type: :controller do
         expect(response).to have_http_status(:success)
       end
 
-      it "assigns Topic.all to topic" do
+      it "assigns Topic.all to topics" do
         get :index
-        expect(assigns(:topics)).to eq([my_topic])
+        expect(assigns(:topics)).to eq([my_topic, my_private_topic])
       end
     end
 
